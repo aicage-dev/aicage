c5545088dbed8d069c3a16cf9d7a93b0e1108c53
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-07-27 21:04:05 +0200

    FORK ONLY: Configure names of local images

--- src/aicage/constants.py
+++ src/aicage/constants.py
@@ -14,11 +14,11 @@ VERSION_CHECK_IMAGE: str = "ghcr.io/aicage/aicage-image-util:agent-version"
 
 # ### END: FORK IMAGES ###
 
-LOCAL_IMAGE_BASE_REPOSITORY: str = "aicage-image-base"
-LOCAL_IMAGE_REPOSITORY: str = "aicage"
+LOCAL_IMAGE_BASE_REPOSITORY: str = "aicage-image-base-dev"
+LOCAL_IMAGE_REPOSITORY: str = "aicage-dev"
 
 DEFAULT_IMAGE_BASE: str = "ubuntu"
-DEFAULT_EXTENDED_IMAGE_NAME: str = "aicage-extended"
+DEFAULT_EXTENDED_IMAGE_NAME: str = "aicage-extended-dev"
 
 IMAGE_REGISTRY: str = "ghcr.io"
 _IMAGE_REGISTRY_API_URL: str = "https://ghcr.io/v2"
